10065ce09af9b158fcbd6a6e8707d485904e5437
venomdesign <chris.schweigart@venomdesign.com>
2026-06-17 18:40:45 -0400

    fix: form components showcase pages - add proper form bindings and labels
    - Fix Input component template with proper [formControl] bindings and () pipe
    - Add missing labels to all form validation examples
    - Change disabled attribute to property binding [disabled]='true'
    - Add FormBuilder setup in Input component with proper form controls
    - All form components now have labels, proper bindings, and working validation

--- projects/showcase/src/app/pages/components/forms/input/input-docs.ts
+++ projects/showcase/src/app/pages/components/forms/input/input-docs.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UsliInputComponent } from 'ui-sdk';
 
 @Component({
   selector: 'app-input-docs',
   standalone: true,
-  imports: [UsliInputComponent],
+  imports: [UsliInputComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './input-docs.html',
   styleUrl: './input-docs.scss',
 })
-export class InputDocs {}
+export class InputDocs {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      required: new FormControl('', Validators.required),
+      minmax: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+      formBound: new FormControl('', Validators.required),
+      twoWay: new FormControl(''),
+      errorEmail: new FormControl('', [Validators.required, Validators.email]),
+      disabled: new FormControl({ value: '', disabled: true }),
+    });
+  }
+}
