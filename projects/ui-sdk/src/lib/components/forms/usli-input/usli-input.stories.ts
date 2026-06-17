22b05b349e788cab2a324ad86d0c6319e1d25d8b
venomdesign <chris.schweigart@venomdesign.com>
2026-06-17 13:27:24 -0400

    feat: add storybook stories for all form components

---
+++ projects/ui-sdk/src/lib/components/forms/usli-input/usli-input.stories.ts
@@ -0,0 +1,53 @@
+import { Meta, StoryObj } from '@storybook/angular';
+import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+import { UsliInputComponent } from './usli-input.component';
+
+const meta: Meta<UsliInputComponent> = {
+  component: UsliInputComponent,
+  title: 'Form Components/Input',
+  tags: ['autodocs'],
+  imports: [ReactiveFormsModule],
+};
+
+export default meta;
+type Story = StoryObj<UsliInputComponent>;
+
+export const Default: Story = {
+  render: (args) => ({
+    props: { ...args, ctrl: new FormControl('') },
+    template: `<usli-input [formControl]="ctrl" type="text" placeholder="Enter text" />`,
+  }),
+};
+
+export const WithLabel: Story = {
+  render: (args) => ({
+    props: { ...args, ctrl: new FormControl('') },
+    template: `
+      <usli-form-field label="Email">
+        <usli-input [formControl]="ctrl" type="email" placeholder="your@email.com" />
+      </usli-form-field>
+    `,
+  }),
+};
+
+export const WithValidation: Story = {
+  render: (args) => ({
+    props: { ...args, ctrl: new FormControl('', [Validators.required, Validators.email]) },
+    template: `
+      <usli-form-field label="Email">
+        <usli-input [formControl]="ctrl" type="email" placeholder="your@email.com" />
+      </usli-form-field>
+    `,
+  }),
+};
+
+export const Password: Story = {
+  render: (args) => ({
+    props: { ...args, ctrl: new FormControl('') },
+    template: `
+      <usli-form-field label="Password">
+        <usli-input [formControl]="ctrl" type="password" placeholder="••••••••" />
+      </usli-form-field>
+    `,
+  }),
+};
