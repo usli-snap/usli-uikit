be41030d2f8999fd4625d68ddd6fdb564ad3f480
venomdesign <chris.schweigart@venomdesign.com>
2026-06-17 13:34:05 -0400

    Add components

--- projects/ui-sdk/src/lib/components/forms/usli-select/usli-select.stories.ts
+++ projects/ui-sdk/src/lib/components/forms/usli-select/usli-select.stories.ts
@@ -1,4 +1,4 @@
-import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
+import { Meta, StoryObj } from '@storybook/angular';
 import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
 import { UsliSelectComponent } from './usli-select.component';
 
@@ -6,7 +6,7 @@ const meta: Meta<UsliSelectComponent> = {
   component: UsliSelectComponent,
   title: 'Form Components/Select',
   tags: ['autodocs'],
-  decorators: [moduleMetadata({ imports: [ReactiveFormsModule] })],
+  imports: [ReactiveFormsModule],
 };
 
 export default meta;
@@ -14,12 +14,16 @@ type Story = StoryObj<UsliSelectComponent>;
 
 export const Default: Story = {
   render: (args) => ({
-    props: { ...args, ctrl: new FormControl('') },
+    props: {
+      ...args,
+      ctrl: new FormControl(''),
+    },
     template: `
       <usli-select [formControl]="ctrl">
         <option value="">Choose an option</option>
         <option value="option1">Option 1</option>
         <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
       </usli-select>
     `,
   }),
@@ -27,30 +31,75 @@ export const Default: Story = {
 
 export const WithLabel: Story = {
   render: (args) => ({
-    props: { ...args, ctrl: new FormControl('') },
+    props: {
+      ...args,
+      ctrl: new FormControl(''),
+    },
     template: `
       <usli-form-field label="Select an option">
         <usli-select [formControl]="ctrl">
           <option value="">-- Please select --</option>
           <option value="apple">Apple</option>
           <option value="banana">Banana</option>
+          <option value="orange">Orange</option>
         </usli-select>
       </usli-form-field>
     `,
+    imports: [...(meta.imports || [])],
   }),
 };
 
 export const WithValidation: Story = {
   render: (args) => ({
-    props: { ...args, ctrl: new FormControl('', Validators.required) },
+    props: {
+      ...args,
+      ctrl: new FormControl('', Validators.required),
+    },
     template: `
       <usli-form-field label="Country">
         <usli-select [formControl]="ctrl">
           <option value="">Select a country</option>
           <option value="us">United States</option>
           <option value="uk">United Kingdom</option>
+          <option value="ca">Canada</option>
         </usli-select>
       </usli-form-field>
     `,
+    imports: [...(meta.imports || [])],
+  }),
+};
+
+export const Disabled: Story = {
+  render: (args) => ({
+    props: {
+      ...args,
+      ctrl: new FormControl({ value: 'option1', disabled: true }),
+    },
+    template: `
+      <usli-select [formControl]="ctrl">
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+      </usli-select>
+    `,
+  }),
+};
+
+export const Multiple: Story = {
+  render: (args) => ({
+    props: {
+      ...args,
+      ctrl: new FormControl(''),
+    },
+    template: `
+      <usli-form-field label="Select technologies">
+        <usli-select [formControl]="ctrl" multiple>
+          <option value="angular">Angular</option>
+          <option value="react">React</option>
+          <option value="vue">Vue</option>
+          <option value="svelte">Svelte</option>
+        </usli-select>
+      </usli-form-field>
+    `,
+    imports: [...(meta.imports || [])],
   }),
 };
--- projects/ui-sdk/src/lib/components/forms/usli-textarea/usli-textarea.stories.ts
+++ projects/ui-sdk/src/lib/components/forms/usli-textarea/usli-textarea.stories.ts
@@ -1,4 +1,4 @@
-import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
+import { Meta, StoryObj } from '@storybook/angular';
 import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
 import { UsliTextareaComponent } from './usli-textarea.component';
 
@@ -6,7 +6,7 @@ const meta: Meta<UsliTextareaComponent> = {
   component: UsliTextareaComponent,
   title: 'Form Components/Textarea',
   tags: ['autodocs'],
-  decorators: [moduleMetadata({ imports: [ReactiveFormsModule] })],
+  imports: [ReactiveFormsModule],
 };
 
 export default meta;
@@ -14,40 +14,88 @@ type Story = StoryObj<UsliTextareaComponent>;
 
 export const Default: Story = {
   render: (args) => ({
-    props: { ...args, ctrl: new FormControl('') },
-    template: `<usli-textarea [formControl]="ctrl" placeholder="Enter your message" [rows]="4" />`,
+    props: {
+      ...args,
+      ctrl: new FormControl(''),
+    },
+    template: `
+      <usli-textarea
+        [formControl]="ctrl"
+        placeholder="Enter your message"
+        [rows]="4"
+      />
+    `,
   }),
 };
 
 export const WithLabel: Story = {
   render: (args) => ({
-    props: { ...args, ctrl: new FormControl('') },
+    props: {
+      ...args,
+      ctrl: new FormControl(''),
+    },
     template: `
       <usli-form-field label="Comments">
-        <usli-textarea [formControl]="ctrl" placeholder="Enter your comments" [rows]="4" />
+        <usli-textarea
+          [formControl]="ctrl"
+          placeholder="Enter your comments"
+          [rows]="4"
+        />
       </usli-form-field>
     `,
+    imports: [...(meta.imports || [])],
   }),
 };
 
 export const WithValidation: Story = {
   render: (args) => ({
-    props: { ...args, ctrl: new FormControl('', [Validators.required, Validators.minLength(10)]) },
+    props: {
+      ...args,
+      ctrl: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    },
     template: `
       <usli-form-field label="Description">
-        <usli-textarea [formControl]="ctrl" placeholder="Enter at least 10 characters" [rows]="5" />
+        <usli-textarea
+          [formControl]="ctrl"
+          placeholder="Enter at least 10 characters"
+          [rows]="5"
+        />
       </usli-form-field>
     `,
+    imports: [...(meta.imports || [])],
   }),
 };
 
 export const LargeTextarea: Story = {
   render: (args) => ({
-    props: { ...args, ctrl: new FormControl('') },
+    props: {
+      ...args,
+      ctrl: new FormControl(''),
+    },
     template: `
       <usli-form-field label="Full Description">
-        <usli-textarea [formControl]="ctrl" placeholder="Enter detailed description" [rows]="8" />
+        <usli-textarea
+          [formControl]="ctrl"
+          placeholder="Enter detailed description"
+          [rows]="8"
+        />
       </usli-form-field>
+    `,
+    imports: [...(meta.imports || [])],
+  }),
+};
+
+export const Disabled: Story = {
+  render: (args) => ({
+    props: {
+      ...args,
+      ctrl: new FormControl({ value: 'This textarea is disabled', disabled: true }),
+    },
+    template: `
+      <usli-textarea
+        [formControl]="ctrl"
+        [rows]="4"
+      />
     `,
   }),
 };
